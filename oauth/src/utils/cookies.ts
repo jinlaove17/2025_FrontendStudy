018f457d3b98c61f9410c74391b5829760e71ebe
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-03-03 13:08:33 +0900

    Feat: add default cookie option

--- oauth/src/utils/cookies.ts
+++ oauth/src/utils/cookies.ts
@@ -1,10 +1,17 @@
 import { Cookies } from "react-cookie";
+import { CookieSetOptions } from "universal-cookie";
 
 const cookies = new Cookies();
 
-// 쿠키 설정
-const setCookie = (name: string, value: string, options?: object) => {
-  cookies.set(name, value, { ...options });
+const setCookie = (name: string, value: string, options?: CookieSetOptions) => {
+  const defaultOptions: CookieSetOptions = {
+    path: "/", // 기본적으로 모든 경로에서 쿠키 접근 가능
+    secure: true, // HTTPS 환경에서만 전송(배포 시 필수)
+    sameSite: "strict", // CSRF 방지를 위해 strict 모드 설정
+    ...options,
+  };
+
+  cookies.set(name, value, defaultOptions);
 };
 
 // 쿠키 읽기
