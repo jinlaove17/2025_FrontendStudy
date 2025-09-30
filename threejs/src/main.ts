427f7a9da90024e25d1ef63de9964ad7d3f78e32
JongWoo <jinlaove17@naver.com>
2025-09-30 23:59:01 +0900

    Feat: Add geometry helper class

--- threejs/src/main.ts
+++ threejs/src/main.ts
@@ -1,7 +1,121 @@
-import { OrbitControls } from "three/examples/jsm/Addons.js";
+import {
+  Font,
+  OrbitControls,
+  TextGeometry,
+  TTFLoader,
+} from "three/examples/jsm/Addons.js";
 import "./style.css";
 import * as THREE from "three";
 import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
+
+interface IGeometryHelper {
+  createGeometry: () => THREE.BufferGeometry; // 지오메트리 객체를 생성해서 반환
+  createGUI: (update: () => void) => void; // UI를 생성하는 함수
+}
+
+class BoxGeometryHelper implements IGeometryHelper {
+  private args = {
+    width: 1,
+    height: 1,
+    depth: 1,
+    widthSegments: 1,
+    heightSegments: 1,
+    depthSegments: 1,
+  };
+
+  public createGeometry() {
+    return new THREE.BoxGeometry(
+      this.args.width,
+      this.args.height,
+      this.args.depth,
+      this.args.widthSegments,
+      this.args.heightSegments,
+      this.args.depthSegments
+    );
+  }
+
+  public createGUI(update: () => void) {
+    const gui = new GUI();
+    gui.add(this.args, "width", 0.1, 10, 0.01).onChange(update);
+    gui.add(this.args, "height", 0.1, 10, 0.01).onChange(update);
+    gui.add(this.args, "depth", 0.1, 10, 0.01).onChange(update);
+    gui.add(this.args, "widthSegments", 0.1, 10, 0.01).onChange(update);
+    gui.add(this.args, "heightSegments", 0.1, 10, 0.01).onChange(update);
+    gui.add(this.args, "depthSegments", 0.1, 10, 0.01).onChange(update);
+  }
+}
+
+class CircleGeometryHelper implements IGeometryHelper {
+  private args = {
+    radius: 1,
+    segments: 32,
+    thetaStart: 0,
+    thetaLength: 360,
+  };
+
+  public createGeometry() {
+    return new THREE.CircleGeometry(
+      this.args.radius,
+      this.args.segments,
+      THREE.MathUtils.degToRad(this.args.thetaStart),
+      THREE.MathUtils.degToRad(this.args.thetaLength)
+    );
+  }
+
+  public createGUI(update: () => void) {
+    const gui = new GUI();
+    gui.add(this.args, "radius", 0.1, 1, 0.01).onChange(update);
+    gui.add(this.args, "segments", 1, 64, 1).onChange(update);
+    gui.add(this.args, "thetaStart", 0, 360, 0.1).onChange(update);
+    gui.add(this.args, "thetaLength", 0, 360, 0.1).onChange(update);
+  }
+}
+
+class TextGeometryHelper implements IGeometryHelper {
+  // TextGeometry는 ExtrudeGeometry를 상속 받는 파생 클래스이다.
+  private args = {
+    text: "안녕하세요.",
+    size: 0.5,
+    height: 0.1,
+    curveSegments: 2,
+    bevelSegments: 3,
+    bevelThickness: 0.1,
+    bevelSize: 0.01,
+    bevelOffset: 0,
+    bevelEnabled: true,
+  };
+
+  private font: Font;
+
+  constructor(font: Font) {
+    this.font = font;
+  }
+
+  public createGeometry() {
+    const geometry = new TextGeometry(this.args.text, {
+      ...this.args,
+      font: this.font,
+    });
+
+    geometry.center();
+    geometry.scale(0.1, 0.1, 0.1);
+
+    return geometry;
+  }
+
+  public createGUI(update: () => void) {
+    const gui = new GUI();
+    gui.add(this.args, "text").onChange(update);
+    gui.add(this.args, "size", 0.1, 1, 0.01).onChange(update);
+    gui.add(this.args, "height", 0.1, 1, 0.01).onChange(update);
+    gui.add(this.args, "curveSegments", 1, 32, 1).onChange(update);
+    gui.add(this.args, "bevelSegments", 1, 32, 1).onChange(update);
+    gui.add(this.args, "bevelThickness", 0.01, 1, 0.001).onChange(update);
+    gui.add(this.args, "bevelSize", 0.01, 1, 0.001).onChange(update);
+    gui.add(this.args, "bevelOffset", -1, 1, 0.001).onChange(update);
+    gui.add(this.args, "bevelEnabled").onChange(update);
+  }
+}
 
 class App {
   private renderer: THREE.WebGLRenderer;
@@ -20,6 +134,7 @@ class App {
     this.domApp.appendChild(this.renderer.domElement);
 
     this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.Fog(0x00000000, 1, 3.5); // 씬에 안개를 추가한다.
 
     this.setupCamera();
     this.setupLight();
@@ -50,6 +165,7 @@ class App {
   }
 
   private setupHelper() {
+    // 좌표축에 대한 헬퍼 객체
     const axes = new THREE.AxesHelper(10);
     this.scene.add(axes);
 
@@ -57,7 +173,7 @@ class App {
     this.scene.add(grid);
   }
 
-  private setupModels() {
+  private async setupModels() {
     const meshMaterial = new THREE.MeshPhongMaterial({
       color: 0x156289,
       flatShading: true,
@@ -71,25 +187,14 @@ class App {
       opacity: 0.8,
     });
 
-    const args = {
-      width: 1,
-      height: 2,
-      depth: 1,
-      widthSegments: 1,
-      heightSegments: 2,
-      depthSegments: 1,
-    };
+    // const geometryHelper = new BoxGeometryHelper();
+    // const geometryHelper = new CircleGeometryHelper();
+    const json = await new TTFLoader().loadAsync("./GowunDodum-Regular.ttf");
+    const font = new Font(json);
+    const geometryHelper = new TextGeometryHelper(font);
 
     const createModel = () => {
-      const geometry = new THREE.BoxGeometry(
-        args.width,
-        args.height,
-        args.depth,
-        args.widthSegments,
-        args.heightSegments,
-        args.depthSegments
-      );
-
+      const geometry = geometryHelper.createGeometry();
       const mesh = new THREE.Mesh(geometry, meshMaterial);
       const line = new THREE.LineSegments(
         new THREE.WireframeGeometry(geometry),
@@ -110,14 +215,7 @@ class App {
     };
 
     createModel();
-
-    const gui = new GUI();
-    gui.add(args, "width", 0.1, 10, 0.01).onChange(createModel);
-    gui.add(args, "height", 0.1, 10, 0.01).onChange(createModel);
-    gui.add(args, "depth", 0.1, 10, 0.01).onChange(createModel);
-    gui.add(args, "widthSegments", 0.1, 10, 0.01).onChange(createModel);
-    gui.add(args, "heightSegments", 0.1, 10, 0.01).onChange(createModel);
-    gui.add(args, "depthSegments", 0.1, 10, 0.01).onChange(createModel);
+    geometryHelper.createGUI(createModel);
   }
 
   private setupControl() {
