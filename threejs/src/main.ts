36a70e56e6a9477aeea285a6397df1c703ee40c7
JongWoo <jinlaove17@naver.com>
2025-10-07 23:10:50 +0900

    Feat: Add camera practice

--- threejs/src/main.ts
+++ threejs/src/main.ts
@@ -6,8 +6,7 @@ class App {
   private domApp: Element;
   private renderer: THREE.WebGLRenderer;
   private scene: THREE.Scene;
-  // private camera?: THREE.PerspectiveCamera;
-  private camera?: THREE.OrthographicCamera;
+  private camera?: THREE.PerspectiveCamera;
 
   constructor() {
     this.domApp = document.querySelector("#app")!;
@@ -28,19 +27,7 @@ class App {
     const width = domApp.clientWidth;
     const height = domApp.clientHeight;
 
-    // this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-
-    const aspect = width / height;
-    this.camera = new THREE.OrthographicCamera(
-      -aspect,
-      aspect,
-      1,
-      -1,
-      0.1,
-      100
-    );
-
-    this.camera.zoom = 0.2;
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
     this.camera.position.set(2, 2, 3.5);
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     // new OrbitControls(this.camera, this.domApp as HTMLElement);
@@ -101,6 +88,16 @@ class App {
     smallSpherePivot.position.y = 0.5;
     smallSpherePivot.name = "smallSpherePivot";
 
+    // 새로운 빨간색 구를 추가함
+    const smallSphere2 = new THREE.Object3D();
+    const smallSpherePivot2 = new THREE.Object3D();
+    smallSpherePivot2.add(smallSphere2);
+    bigSphere.add(smallSpherePivot2);
+    smallSphere2.position.x = 2;
+    smallSpherePivot2.rotation.y = THREE.MathUtils.degToRad(-45);
+    smallSpherePivot2.position.y = 0.5;
+    smallSpherePivot2.name = "targetPivot";
+
     const cntItems = 8;
     const geomTorus = new THREE.TorusGeometry(0.3, 0.1);
     const matTorus = new THREE.MeshStandardMaterial({
@@ -133,10 +130,10 @@ class App {
 
     const camera = this.camera;
     if (camera) {
-      // camera.aspect = width / height;
-      const aspect = width / height;
-      camera.left = -aspect;
-      camera.right = aspect;
+      camera.aspect = width / height;
+      // const aspect = width / height;
+      // camera.left = -aspect;
+      // camera.right = aspect;
       camera.updateProjectionMatrix();
     }
 
@@ -152,6 +149,31 @@ class App {
       const euler = new THREE.Euler(0, time, 0);
       const quaterion = new THREE.Quaternion().setFromEuler(euler);
       smallSpherePivot.setRotationFromQuaternion(quaterion);
+
+      const smallSphere = smallSpherePivot.children[0];
+      const smallSpherePos = new THREE.Vector3();
+      smallSphere.getWorldPosition(smallSpherePos);
+
+      // 카메라가 공을 포커스하게 만들기
+      // this.camera?.lookAt(smallSpherePos);
+
+      // 공 시점으로 카메라 배치하기
+      const smallSpherePivot2 = this.scene.getObjectByName("targetPivot");
+      if (smallSpherePivot2) {
+        const euler2 = new THREE.Euler(
+          0,
+          time + THREE.MathUtils.degToRad(10),
+          0
+        );
+        const quaterinon2 = new THREE.Quaternion().setFromEuler(euler2);
+        smallSpherePivot2.setRotationFromQuaternion(quaterinon2);
+
+        const nextPos = smallSpherePivot2.children[0];
+        const cameraTarget = new THREE.Vector3();
+        nextPos.getWorldPosition(cameraTarget);
+        this.camera?.lookAt(cameraTarget);
+        this.camera?.position.copy(smallSpherePos);
+      }
     }
   }
 
