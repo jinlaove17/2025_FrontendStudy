3908c990ce41325632cbb5ac240f2b94c9c6ff8e
JongWoo <jinlaove17@naver.com>
2025-10-07 21:55:27 +0900

    Feat: Add point, spot light

--- threejs/src/main.ts
+++ threejs/src/main.ts
@@ -1,6 +1,7 @@
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import "./style.css";
 import * as THREE from "three";
+import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
 
 class App {
   private domApp: Element;
@@ -8,8 +9,14 @@ class App {
   private scene: THREE.Scene;
   private camera?: THREE.PerspectiveCamera;
 
-  private light?: THREE.DirectionalLight;
-  private lightHelper?: THREE.DirectionalLightHelper;
+  // private light?: THREE.DirectionalLight;
+  // private lightHelper?: THREE.DirectionalLightHelper;
+
+  // private light?: THREE.PointLight;
+  // private lightHelper?: THREE.PointLightHelper;
+
+  private light?: THREE.SpotLight;
+  private lightHelper?: THREE.SpotLightHelper;
 
   constructor() {
     this.domApp = document.querySelector("#app")!;
@@ -39,20 +46,49 @@ class App {
   private setupLight() {
     // 환경광(주변광)
     // const light = new THREE.AmbientLight("#ffffff", 1);
-
     // AmbientLight와 유사하지만 2개의 색상(위, 아래)을 갖는다.
     // const light = new THREE.HemisphereLight("#ff0000", "#0000ff", 5);
 
-    const light = new THREE.DirectionalLight("0xffffff", 1);
-    light.position.set(0, 2, 0);
-    light.rotation.set(10, 0, 0);
-    light.target.position.set(0, 1, 0);
+    // 디렉셔널 라이트
+    // const light = new THREE.DirectionalLight("0xffffff", 1);
+    // light.position.set(0, 2, 0);
+    // light.rotation.set(10, 0, 0);
+    // light.target.position.set(0, 1, 0);
+    // this.light = light;
+    // this.scene.add(light);
+    // const helper = new THREE.DirectionalLightHelper(light);
+    // this.lightHelper = helper;
+    // this.scene.add(helper);
+
+    // 포인트 라이트
+    // const light = new THREE.PointLight(0xffffff, 5);
+    // light.position.set(0, 3, 0);
+    // light.distance = 10; // 광원의 시작 위치부터 지정한 거리까지 영향을 미침(0이면 무한)
+    // this.light = light;
+    // this.scene.add(light);
+    // const helper = new THREE.PointLightHelper(light);
+    // this.lightHelper = helper;
+    // this.scene.add(helper);
+
+    // 스팟 라이트
+    const light = new THREE.SpotLight(0xffffff, 20);
+    light.position.set(0, 5, 0);
+    light.target.position.set(0, 0, 0);
+    light.angle = THREE.MathUtils.degToRad(30);
+    light.penumbra = 0.5; // 가장자리에서 빛의 감쇄율
     this.light = light;
     this.scene.add(light);
+    this.scene.add(light.target);
 
-    const helper = new THREE.DirectionalLightHelper(light);
+    const helper = new THREE.SpotLightHelper(light);
     this.lightHelper = helper;
     this.scene.add(helper);
+
+    const gui = new GUI();
+    gui
+      .add(light, "angle", 0, Math.PI / 2, 0.01)
+      .onChange(() => helper.update());
+    gui.add(light, "penumbra", 0, 1, 0.01).onChange(() => helper.update());
   }
 
   private setupModels() {
@@ -157,6 +193,13 @@ class App {
       //smallSpherePivot.quaternion.setFromEuler(euler);
 
       const smallSphere = smallSpherePivot.children[0];
+      // smallSphere.getWorldPosition(this.light!.target.position);
+      // this.lightHelper?.update();
+
+      // smallSphere.getWorldPosition(this.light!.position);
+      // this.light!.position.y = 1;
+      // this.lightHelper?.update();
+
       smallSphere.getWorldPosition(this.light!.target.position);
       this.lightHelper?.update();
     }
